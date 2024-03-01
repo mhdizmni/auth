f2a980bee12f14d43b0f5c6a25814ac2f367f9f7
mhdizmni <mr.mzamani77@gmail.com>
2024-03-02 00:58:47 +0330

    footer

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,27 +1,14 @@
-import Link from "next/link";
-
 interface AuthLayoutProps {
     children: React.ReactNode
 }
+
 const AuthLayout = ({
     children
 }: AuthLayoutProps) => {
     return (
-        <>
-            <main className="flex min-h-screen flex-col items-center justify-between p-24">
-                {children}
-            </main>
-            <footer
-                className="fixed bottom-0 h-5 bg-sky-700 text-white text-xs w-full flex items-center justify-between px-2"
-            >
-                <div>
-                    © {new Date().getFullYear()} <Link href="https://mitism.com" target="_blank" className="underline">mitism.com</Link>
-                </div>
-                <div>
-                    Developed using <Link href="https://next-auth.js.org/" target="_blank" className="underline italic">NextAuth.js</Link>
-                </div>
-            </footer>
-        </>
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            {children}
+        </main>
     );
 }
  
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import './globals.css'
 import { Providers } from '@/app/providers'
+import Link from 'next/link'
 
 export const metadata: Metadata = {
   title: 'mitism - Auth Demo',
@@ -18,6 +19,16 @@ export default function RootLayout({
         <Providers>
           {children}
         </Providers>
+        <footer
+                className="fixed bottom-0 h-5 bg-sky-700 text-white text-xs w-full flex items-center justify-between px-2"
+            >
+                <div>
+                    © {new Date().getFullYear()} <Link href="https://mitism.com" target="_blank" className="underline">mitism.com</Link>
+                </div>
+                <div>
+                    Developed using <Link href="https://next-auth.js.org/" target="_blank" className="underline italic">NextAuth.js</Link>
+                </div>
+            </footer>
       </body>
     </html>
   )
