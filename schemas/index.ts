0e291828aaad36d3d013850fb879b35d6cb1084b
mhdizmni <mr.mzamani77@gmail.com>
2024-03-01 23:14:09 +0330

    Create index.ts

---
+++ schemas/index.ts
@@ -0,0 +1,28 @@
+import * as z from "zod";
+
+export const LoginSchema = z.object({
+    email: z.string({
+        required_error: "Email is required."
+    }).email({
+        message: "Invalid email address."
+    }),
+    password: z.string().min(1, {
+        message: "Password is required."
+    }),
+})
+
+export const RegisterSchema = z.object({
+    name: z.string({
+        required_error: "Name is required."
+    }).min(1, {
+        message: "Name is required."
+    }),
+    email: z.string({
+        required_error: "Email is required."
+    }).email({
+        message: "Invalid email address."
+    }),
+    password: z.string().min(1, {
+        message: "Password is required."
+    }),
+})
