69b68994ab13a83e92bb0c4cf7302c0fe9bbe0cf
mhdizmni <mr.mzamani77@gmail.com>
2024-03-02 13:06:07 +0330

    Update toast.tsx

--- components/toast.tsx
+++ components/toast.tsx
@@ -14,10 +14,10 @@ export const Toast = ({
 }: ToastProps) => {
     return (
         <div className={cn(
-            "p-3 relative rounded flex items-center gap-x-2 text-sm border",
+            "p-3 relative rounded flex items-center gap-x-2 text-sm border text-ye",
             {"text-destructive border-destructive bg-destructive/15": type === "error"},
-            {"text-emerald/900 border-emerald/900 bg-emerald/15": type === "success"},
-            {"text-yellow border-yellow bg-yellow/15": type === "info"},
+            {"text-emerald-900 border-emerald-900 bg-emerald-200": type === "success"},
+            {"text-yellow-900 border-yellow-900 bg-yellow-200": type === "info"},
         )}>
             {type === "error" && <ExclamationTriangleIcon className="h-4 w-4" />}
             {type === "success" && <CheckCircledIcon className="h-4 w-4" />}
